5a9d7ef76d1d362078069cfda120de88e72d2693
David Francos Cuartero <me@davidfrancos.net>
2020-12-15 00:03:07 +0100

    Update remote plugins

--- docs/install-zshrc.sh
+++ docs/install-zshrc.sh
@@ -2,4 +2,4 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 curl -fLo  ~/.zshrc.pt --create-dirs https://raw.githubusercontent.com/XayOn/prettierTerminal/master/zshrc
 echo "source ~/.zshrc.pt" >> ~/.zshrc
-zsh -c exit
+zsh
